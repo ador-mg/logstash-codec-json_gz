2a1680979b0ce13babe40d4308a152eeccf55e01
Antonis Mygiakis <ador75@gmail.com>
2017-11-13 14:21:22 +0200

    Initial commit

---
+++ lib/logstash/codecs/json_gz.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+require 'logstash/codecs/base'
+
+class LogStash::Codecs::JsonGz < LogStash::Codecs::Base
+  config_name 'json_gz'
+  milestone 1
+
+  public
+  def register
+    require 'zlib'
+    require 'stringio'
+  end
+
+  def decode(data)
+    begin
+      json_data = Zlib::GzipReader.new(StringIO.new(data)) { |f| f.read }
+    rescue Zlib::GzipFile::Error => e
+      @logger.info('Gzip failure, probably not zipped', :error => e, :data => json_data)
+      json_data = data
+    end
+
+    begin
+      yield LogStash::Event.new(JSON.parse(json_data)) if json_data
+    rescue JSON::ParserError => e
+      @logger.info('JSON parse failure. Falling back to plain-text', :error => e, :data => json_data)
+      yield LogStash::Event.new('message' => json_data)
+    end
+  end # def decode
+
+  def encode(data)
+    raise NotImplementedError
+  end # def encode
+
+end # class LogStash::Codecs::JsonGz
