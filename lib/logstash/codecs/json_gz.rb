473dc51e40e3003c466714de011886fc5f583b87
Antonis Mygiakis <ador75@gmail.com>
2017-11-17 17:05:22 +0200

    Inject the json into the Event's message

--- lib/logstash/codecs/json_gz.rb
+++ lib/logstash/codecs/json_gz.rb
@@ -20,14 +20,14 @@ class LogStash::Codecs::JsonGz < LogStash::Codecs::Base
       gz = Zlib::GzipReader.new(rawdata)
       json_data = gz.read
     rescue Zlib::GzipFile::Error => e
-      @logger.info('Gzip failure, probably not zipped', :error => e, :data => data)
+      @logger.info('Gzip failure, PROBABLY not zipped. Falling back to plain text...', :error => e, :data => data)
       json_data = data
     end
 
     begin
       @logger.info('LogStash::Codecs::JsonGz: after zipping part. ', :data => json_data)
 
-      yield LogStash::Event.new(LogStash::Json.load(json_data)) if json_data
+      yield LogStash::Event.new('message' => LogStash::Json.load(json_data)) if json_data
     rescue LogStash::Json::ParserError => e
       @logger.info('LogStash::Json parse failure. Falling back to plain-text', :error => e, :data => json_data)
       yield LogStash::Event.new('message' => json_data)
